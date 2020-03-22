3502f0c8894f78b158c7245c506c058dc56c556a
sergey-selivanov <sergey_selivanov@hotmail.com>
2020-03-22 05:00:29 +0200

    some notes, update libs

---
+++ doc/convert.cmd
@@ -0,0 +1,2 @@
+rem imagemagick
+c:convert amor.png -define icon:autoresize amor.ico
---
+++ doc/cookbook.mm
@@ -0,0 +1,17 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1584842156287" ID="ID_883179019" MODIFIED="1584842164774" TEXT="Cookbook">
+<node CREATED="1584842195536" ID="ID_1126533596" MODIFIED="1584842198865" POSITION="right" TEXT="TODO">
+<node CREATED="1584842199814" ID="ID_1884996938" MODIFIED="1584842913740" TEXT="try jsoup to parse and modify html? instead of xerces">
+<node CREATED="1584842301766" ID="ID_1567968615" MODIFIED="1584842303903" TEXT="Sanitize untrusted HTML"/>
+</node>
+<node CREATED="1584842918172" ID="ID_1539987114" MODIFIED="1584842934537" TEXT="produce installer"/>
+<node CREATED="1584843045443" ID="ID_562658722" MODIFIED="1584843065501" TEXT="use hibernate"/>
+<node CREATED="1584843066257" ID="ID_45678986" MODIFIED="1584843071307" TEXT="use flywaydb"/>
+</node>
+<node CREATED="1584842230232" ID="ID_1666087264" MODIFIED="1584842233213" POSITION="left" TEXT="Links">
+<node CREATED="1584842323660" ID="ID_1566718536" LINK="https://jsoup.org/cookbook/introduction/parsing-a-document" MODIFIED="1584842373104" TEXT="https://jsoup.org/cookbook/introduction/parsing-a-document"/>
+<node CREATED="1584844571104" ID="ID_1065359523" LINK="https://www.favicon-generator.org/" MODIFIED="1584844584211" TEXT="https://www.favicon-generator.org/"/>
+</node>
+</node>
+</map>
